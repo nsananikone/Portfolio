2be774115704ba4ff481dd147d07fe8f2ddf3242
nsananikone <nsananikone1134@gmail.com>
2022-07-12 15:31:07 -0500

    Add email link to side bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,14 +107,16 @@ export const Navbar = () => {
               Github <FaGithub size={30} />
             </a>
           </li>
+
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="mailto:nsananikone1134@gmail.org"
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
+
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
