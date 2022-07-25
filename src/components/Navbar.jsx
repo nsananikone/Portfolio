d996193318d9c5c1a0fc79a48f1c30449eb61aee
nsananikone <nsananikone1134@gmail.com>
2022-07-25 11:40:37 -0500

    Updated LinkedIn link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ export const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/nick-sananikone-0a19ba243"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
